76b49c3182fd28dd40b20e6f799da9bfafe566a2
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-06-18 15:27:20 +0100

    Added Soran STT as a submodule

--- speech_in.py
+++ speech_in.py
@@ -1,8 +1,9 @@
 import subprocess
+from config.tensorflowasr import *
 
 
 def listen():
-    subprocess.call(['rec rec.wav rate 32k silence 1 0.1 5% 1 1.0 5%'], shell=True)
+    subprocess.call([f'rec {audio_file} rate 32k silence 1 0.1 5% 1 1.0 5%'], shell=True)
 
 
 if __name__ == '__main__':
