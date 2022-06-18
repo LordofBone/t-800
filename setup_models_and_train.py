c801444ce9f8f897a79e6afb4c2ac34fbf27ca64
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-06-18 20:29:43 +0100

    Added model setup/trainer, now with model download for tensorflowasr

--- setup_models_and_train.py
+++ setup_models_and_train.py
@@ -1,6 +1,9 @@
+from soran.utils.model_downloader import download_models
+
 from Bot_Engine.utils.sentiment_training_suite import train_all
 
 from Bot_Engine.Chatbot_8.bot_8_trainer import bot_trainer
 
 bot_trainer()
 train_all()
+download_models()
