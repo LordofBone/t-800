bced57010e73ed4ea767d8c651c93328de039c6b
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-06-27 23:20:48 +0100

    Removed LED controller and moved functionality to pi_operations.py also made this use the event queue for hardware access

--- hardware/pi_operations.py
+++ hardware/pi_operations.py
@@ -5,8 +5,7 @@
 from events.event_queue import EventQueueAccess
 
 from time import sleep
-
-import subprocess
+from subprocess import call
 
 import logging
 
@@ -16,7 +15,7 @@ logger = logging.getLogger("pi-operations")
 class PiOperations:
     def __init__(self):
         # todo: add extra raspberry pi operations here, temp checks etc.
-        pass
+        self.led_flash_default = 3
 
     def shutdown(self):
         """
@@ -24,7 +23,7 @@ class PiOperations:
         :return:
         """
         logger.debug("Switching Off")
-        subprocess.call('sudo shutdown now', shell=True)
+        call('sudo shutdown now', shell=True)
 
     def reboot(self):
         """
@@ -32,9 +31,46 @@ class PiOperations:
         :return:
         """
         logger.debug("Restarting")
-        subprocess.call('sudo reboot now', shell=True)
+        call('sudo reboot now', shell=True)
+
+    def led_on(self):
+        """
+        Turn the LED on
+        :return:
+        """
+        logger.debug("Turning LED on")
+        call('echo "79" > /sys/class/gpio/export', shell=True)
+        call('echo "out" > /sys/class/gpio/gpio79/direction', shell=True)
+        call('echo "1" > /sys/class/gpio/gpio79/value', shell=True)
+
+    def led_off(self):
+        """
+        Turn the LED off
+        :return:
+        """
+        logger.debug("Turning LED off")
+        call('echo "79" > /sys/class/gpio/export', shell=True)
+        call('echo "out" > /sys/class/gpio/gpio79/direction', shell=True)
+        call('echo "0" > /sys/class/gpio/gpio79/value', shell=True)
+
+    def led_flash(self, times_to_flash):
+        """
+        Flash the LED a number of times
+        :param times_to_flash:
+        :return:
+        """
+        logger.debug("Flashing LED")
+        for i in range(times_to_flash):
+            call('echo "0" > /sys/class/gpio/gpio79/value', shell=True)
+            sleep(0.1)
+            call('echo "1" > /sys/class/gpio/gpio79/value', shell=True)
+            sleep(0.1)
 
     def queue_checker(self):
+        """
+        Check the event queue for events
+        :return:
+        """
         while True:
             event = EventQueueAccess.get_latest_event(["HARDWARE_PI"])
             if event:
@@ -42,6 +78,12 @@ class PiOperations:
                     self.shutdown()
                 elif event[1] == "REBOOT":
                     self.reboot()
+                elif event[1] == "LED_ON":
+                    self.led_on()
+                elif event[1] == "LED_OFF":
+                    self.led_off()
+                elif event[1] == "LED_FLASH":
+                    self.led_flash(self.led_flash_default)
             else:
                 sleep(1)
 
@@ -49,5 +91,7 @@ class PiOperations:
 PiOperationsAccess = PiOperations()
 
 if __name__ == "__main__":
-    # Test the OS calls work with a reboot
-    PiOperationsAccess.reboot()
+    """
+    Run the PiOperations class to test LED function
+    """
+    PiOperationsAccess.led_flash(3)
