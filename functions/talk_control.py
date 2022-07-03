6074fbfaf1c0bf4ba1ba704bf890c789128a4c22
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-07-03 18:49:55 +0100

    Added extra function for talking via tts and an event for activating TTS output

--- functions/talk_control.py
+++ functions/talk_control.py
@@ -2,9 +2,11 @@ from soran.integrate_stt import SpeechtoTextHandler
 
 from Bot_Engine.functions import core_systems
 
-from events.event_queue import EventQueueAccess, DrawListQueueAccess
+from events.event_queue import EventQueueAccess
 
-from events.event_types import LISTEN_STT, TALK_SYSTEMS, OVERLAY_DRAW, LISTENING, INFERENCING_SPEECH
+from Bot_Engine.functions.voice_controller import VoiceControllerAccess
+
+from events.event_types import LISTEN_STT, TALK_SYSTEMS, SPEAK_TTS
 
 from hardware.pi_operations import *
 
@@ -35,6 +37,13 @@ class TalkController:
         """
         self.interrupt = True
 
+    def speak_tts(self, text_in):
+        """
+        This function is used to speak the bot response.
+        :return:
+        """
+        VoiceControllerAccess.tts(text_in)
+
     def listen_stt(self):
         """
         This is the main function that runs the STT and bot interaction.
@@ -44,23 +53,21 @@ class TalkController:
 
         self.inference_output = self.STT_handler.run_inference()
 
+        self.command_checker()
+
         logger.debug(self.inference_output)
 
         self.bot_response = (BotControl.input_get_response(self.inference_output))
 
         logger.debug(self.bot_response)
 
-        # todo: ensure this works properly, should be checking inputs for commands not bot responses
-        self.command_checker()
-
     def command_checker(self):
         """
         This function checks if the bot response is a command.
         :return:
         """
-        pass
-        # if self.bot_response == "shut down":
-        #     EventQueueAccess().add_event(SHUTDOWN)
+        if self.inference_output == "shut down":
+            EventQueueAccess().add_event(SHUTDOWN)
 
     def queue_checker(self):
         """
@@ -69,17 +76,12 @@ class TalkController:
         """
         while True:
             event = EventQueueAccess.get_latest_event([TALK_SYSTEMS])
+            split_event_details = event[2].split("|")
             if event:
-                if event[2] == LISTEN_STT:
+                if split_event_details[0] == LISTEN_STT:
                     self.listen_stt()
-                # elif event[1] == REBOOT:
-                #     self.reboot()
-                # elif event[1] == LED_ON:
-                #     self.led_on()
-                # elif event[1] == LED_OFF:
-                #     self.led_off()
-                # elif event[1] == LED_FLASH:
-                #     self.led_flash(self.led_flash_default)
+                elif split_event_details[0] == SPEAK_TTS:
+                    self.speak_tts(split_event_details[1])
             else:
                 sleep(1)
 
