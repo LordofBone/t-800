07c52c1290f0a42ee346fc70c1c0b13bf9597497
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-07-16 16:06:39 +0100

    Added comment explaining module

--- functions/speech_in.py
+++ functions/speech_in.py
@@ -2,6 +2,9 @@ import subprocess
 from config.tensorflowasr import *
 
 
+# currently not used as soran is handling the speech to text recording entirely, may be used to implement a speech to
+# text in future to allow for a dynamic time recording
+
 def listen():
     """
     Listen to the audio input and return the text
