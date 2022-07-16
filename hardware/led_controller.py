3134ea6608f031c01d5f4839395ab513ee9a12d9
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-07-16 12:30:34 +0100

    Event type added for LED controller also split out LED controls from pioperations

---
+++ hardware/led_controller.py
@@ -0,0 +1,80 @@
+from events.event_queue import EventQueueAccess
+from events.event_types import LED_ON, LED_OFF, LED_FLASH, LED_ACCESS
+
+from time import sleep
+
+import logging
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+ledPin = 8
+GPIO.setup(ledPin, GPIO.OUT)
+
+logger = logging.getLogger("led-controller")
+
+
+class LEDController:
+    def __init__(self):
+        self.led_flash_default = 3
+
+    def led_on(self):
+        """
+        Turn the LED on
+        :return:
+        """
+        logger.debug("Turning LED on")
+        logger.debug("Eye LED turning on")
+        GPIO.output(ledPin, GPIO.HIGH)
+
+    def led_off(self):
+        """
+        Turn the LED off
+        :return:
+        """
+        logger.debug("Turning LED off")
+        logger.debug("Eye LED turning off")
+        GPIO.output(ledPin, GPIO.LOW)
+
+    def led_flash(self, times_to_flash):
+        """
+        Flash the LED a number of times
+        :param times_to_flash:
+        :return:
+        """
+        logger.debug("Flashing LED")
+        for i in range(times_to_flash):
+            self.led_on()
+            time.sleep(0.5)
+            logger.debug("Eye LED turning off")
+            self.led_off()
+            time.sleep(0.5)
+
+    def queue_checker(self):
+        """
+        Check the event queue for events
+        :return:
+        """
+        while True:
+            event = EventQueueAccess.get_latest_event([LED_ACCESS])
+            if event:
+                if event[1] == LED_ON:
+                    self.led_on()
+                elif event[1] == LED_OFF:
+                    self.led_off()
+                elif event[1] == LED_FLASH:
+                    self.led_flash(self.led_flash_default)
+            else:
+                sleep(1)
+
+
+LEDControllerAccess = LEDController()
+
+if __name__ == "__main__":
+    """
+    Run the LEDControllerAccess class to test LED function
+    """
+    LEDControllerAccess.led_flash(3)
+    GPIO.cleanup()
--- hardware/pi_operations.py
+++ hardware/pi_operations.py
@@ -1,17 +1,12 @@
-# !/user/bin/env python3
-
-# This module is for operating system level operations on the Pi, such as shutdowns, reboots etc.
-
 from events.event_queue import EventQueueAccess
-from events.event_types import SHUTDOWN, REBOOT, LED_ON, LED_OFF, LED_FLASH, HARDWARE_PI
+from events.event_types import SHUTDOWN, REBOOT, HARDWARE_PI
 
-from time import sleep
 from subprocess import call
 
 import logging
 
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -24,14 +19,15 @@ logger = logging.getLogger("pi-operations")
 class PiOperations:
     def __init__(self):
         # todo: add extra raspberry pi operations here, temp checks etc.
-        self.led_flash_default = 3
+        self.shutdown_wait_seconds = 5
 
     def shutdown(self):
         """
         Shutdown the Pi
         :return:
         """
-        logger.debug("Switching Off")
+        logger.debug(f"Switching Off in {self.shutdown_wait_seconds} seconds")
+        sleep(self.shutdown_wait_seconds)
         call('sudo shutdown now', shell=True)
 
     def reboot(self):
@@ -39,40 +35,9 @@ class PiOperations:
         Restart the Pi
         :return:
         """
-        logger.debug("Restarting")
+        logger.debug(f"Restarting in {self.shutdown_wait_seconds} seconds")
+        sleep(self.shutdown_wait_seconds)
         call('sudo reboot now', shell=True)
-
-    def led_on(self):
-        """
-        Turn the LED on
-        :return:
-        """
-        logger.debug("Turning LED on")
-        logger.debug("Eye LED turning on")
-        GPIO.output(ledPin, GPIO.HIGH)
-
-    def led_off(self):
-        """
-        Turn the LED off
-        :return:
-        """
-        logger.debug("Turning LED off")
-        logger.debug("Eye LED turning off")
-        GPIO.output(ledPin, GPIO.LOW)
-
-    def led_flash(self, times_to_flash):
-        """
-        Flash the LED a number of times
-        :param times_to_flash:
-        :return:
-        """
-        logger.debug("Flashing LED")
-        for i in range(times_to_flash):
-            self.led_on()
-            time.sleep(0.5)
-            logger.debug("Eye LED turning off")
-            self.led_off()
-            time.sleep(0.5)
 
     def queue_checker(self):
         """
@@ -86,12 +51,6 @@ class PiOperations:
                     self.shutdown()
                 elif event[1] == REBOOT:
                     self.reboot()
-                elif event[1] == LED_ON:
-                    self.led_on()
-                elif event[1] == LED_OFF:
-                    self.led_off()
-                elif event[1] == LED_FLASH:
-                    self.led_flash(self.led_flash_default)
             else:
                 sleep(1)
 
@@ -102,4 +61,4 @@ if __name__ == "__main__":
     """
     Run the PiOperations class to test LED function
     """
-    PiOperationsAccess.led_flash(3)
+    PiOperationsAccess.shutdown()
